05c782e98779bec6c5e92f07fa754f6b1b625d25
sjrnf <sjrnf@DESKTOP-5BRA6RF>
2025-11-25 17:26:36 +0900

    1.2업데이트

--- Fspringproject/src/main/java/org/embed/FspringprojectApplication.java
+++ Fspringproject/src/main/java/org/embed/FspringprojectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FspringprojectApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(FspringprojectApplication.class, args);
 	}
